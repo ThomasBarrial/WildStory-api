f617e42badded3fe7cc6f49ddba74206dfa8aeda
Thomas <barrialthomas@gmail.com>
2021-09-22 16:33:06 +0200

    eslint condig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,29 @@
 module.exports = {
   env: {
-    commonjs: true,
     es2021: true,
     node: true,
     jest: true,
+    commonjs: true,
   },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
   ],
-  rules: {
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
-    'no-console': 1,
-  },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    allowImportExportEverywhere: true,
     sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', ignoreRestSiblings: true },
+    ],
+    'no-console': 1,
   },
 };
