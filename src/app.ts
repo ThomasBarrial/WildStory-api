43df0e06243376b31cfe968fcb3802d3c9aadf8a
Thomas <thomas@barrial.fr>
2021-10-31 15:15:39 +0100

    solveCors

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,7 @@ import { notFound, errorHandler } from './middlewares';
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: '*',
-  })
-);
+app.use(cors());
 app.use(helmet());
 
 if (process.env.NODE_ENV !== 'test') {
